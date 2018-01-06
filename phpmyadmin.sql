c63cc5455718e128e628986e755d8b1329c245fe
Bob Ballantyne <bob.ballantyne@emc.com>
2018-01-06 12:39:30 -0500

    mysql DB updates

--- phpmyadmin.sql
+++ phpmyadmin.sql
@@ -251,7 +251,7 @@ CREATE TABLE `pma__recent` (
 
 LOCK TABLES `pma__recent` WRITE;
 /*!40000 ALTER TABLE `pma__recent` DISABLE KEYS */;
-INSERT INTO `pma__recent` VALUES ('bobb','[{\"db\":\"nconf\",\"table\":\"ItemLinks\"},{\"db\":\"nconf\",\"table\":\"ConfigAttrs\"}]');
+INSERT INTO `pma__recent` VALUES ('bobb','[{\"db\":\"grafana\",\"table\":\"org_user\"},{\"db\":\"grafana\",\"table\":\"org\"},{\"db\":\"grafana\",\"table\":\"user\"}]');
 /*!40000 ALTER TABLE `pma__recent` ENABLE KEYS */;
 UNLOCK TABLES;
 
